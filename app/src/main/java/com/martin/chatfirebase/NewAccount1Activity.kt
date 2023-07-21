64c5f0782f859a19b155de1c64ff0ff43402f862
rkdms <rkdms4971@naver.com>
2023-07-21 17:08:19 +0900

    FireChat

---
+++ app/src/main/java/com/martin/chatfirebase/NewAccount1Activity.kt
@@ -0,0 +1,11 @@
+package com.martin.chatfirebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewAccount1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_newaccount1)
+    }
+}
