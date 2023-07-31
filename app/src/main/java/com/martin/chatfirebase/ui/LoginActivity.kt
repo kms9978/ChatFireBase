5314cc052eecc080d564d078c0b8000b3bb17f4d
rkdms <rkdms4971@naver.com>
2023-07-31 21:32:06 +0900

    FireChat

--- app/src/main/java/com/martin/chatfirebase/ui/LoginActivity.kt
+++ app/src/main/java/com/martin/chatfirebase/ui/LoginActivity.kt
@@ -1,5 +1,4 @@
 package com.martin.chatfirebase.ui
-
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -23,28 +22,28 @@ class LoginActivity: AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        binding.signUpButton.setOnClickListener {
-//            val email = binding.emailEditText.text.toString()
-//            val password = binding.passwordEditText.text.toString()
-//
-//            if (email.isEmpty() || password.isEmpty()) {
-//                Toast.makeText(this, "이메일 또는 패스워드가 입력되지 않았습니다.", Toast.LENGTH_SHORT).show()
-//                return@setOnClickListener
-//            }
-//
-//            Firebase.auth.createUserWithEmailAndPassword(email, password)
-//                .addOnCompleteListener(this) { task ->
-//                    if (task.isSuccessful) {
-//                        // 회원가입 성공
-//                        Toast.makeText(this, "회원가입에 성공했습니다. 로그인해주세요.", Toast.LENGTH_SHORT).show()
-//                    } else {
-//                        // 회원가입 실패
-//                        Toast.makeText(this, "회원가입에 실패했습니다.", Toast.LENGTH_SHORT).show()
-//                    }
-//
-//                }
-//
-//        }
+        binding.signUpButton.setOnClickListener {
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "이메일 또는 패스워드가 입력되지 않았습니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            Firebase.auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // 회원가입 성공
+                        Toast.makeText(this, "회원가입에 성공했습니다. 로그인해주세요.", Toast.LENGTH_SHORT).show()
+                    } else {
+                        // 회원가입 실패
+                        Toast.makeText(this, "회원가입에 실패했습니다.", Toast.LENGTH_SHORT).show()
+                    }
+
+                }
+
+        }
 
         binding.loginBtn.setOnClickListener {
             val email = binding.emailEditText.text.toString()
